c3143339d2522ca2b7a53fb92270ca018fb6cc10
Andrey <cors223@gmail.com>
2021-02-07 20:36:36 +0300

    add a soluttions

--- work1.py
+++ work1.py
@@ -1,1 +1,12 @@
+def division (*args):
+    try:
+        number1 = int(input(f"Enter the first number: "))
+        number2 = int(input(f"Enter the second number: "))
+        res = number1 / number2
+    except BaseException:
+        return ("ERROR. EXIT.")
+        exit(-1)
 
+    return int(res)
+
+print(division())
---
+++ work6.py
@@ -0,0 +1,6 @@
+def int_func (*args):
+    word = input(f"Enter your text: ")
+    print(word.title())
+    return
+
+int_func()
